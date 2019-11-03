efa83fbda65bda306cdb601baa3a665d6a8d2304
Jorge <m.jorge1slv@yahoo.es>
2019-11-02 19:35:22 -0600

    ahora ya se pueden agregar los datos de humedad y luz desde el endpoint de php

--- paginas/php/enviarDatos.php
+++ paginas/php/enviarDatos.php
@@ -2,7 +2,11 @@
 	require_once('../../conexion/conexion.php');
 	$conexion = conectar();
 	$temperatura = $_GET['temperatura'];
-	$sqlAgregarTemperatura = "INSERT INTO temperaturasdiarias (Temperatura) VALUES ($temperatura)";
-	mysqli_query($conexion, $sqlAgregarTemperatura);
+	$humedad = $_GET['humedad'];
+	$luz = $_GET['luz'];
+	$sqlAgregarTemperatura = "INSERT INTO temperaturasdiarias (Temperatura, humedad, luz) VALUES ($temperatura, $humedad,$luz)";
+	if (mysqli_query($conexion, $sqlAgregarTemperatura)) {
+		echo "Datos agregados con exito";
+	}
     mysqli_close($conexion);
 ?>
