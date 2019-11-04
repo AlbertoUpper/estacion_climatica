9a96c8b354ab245adf667a07239626260676615f
Jorge <m.jorge1slv@yahoo.es>
2019-11-04 17:42:46 -0600

    se agrego el proceso de interpolacion y los valores promedio necesarios de las mediciones

--- paginas/inicio.php
+++ paginas/inicio.php
@@ -1,4 +1,5 @@
 <?php 
+	date_default_timezone_set('America/El_Salvador');
 	require_once('conexion/conexion.php');
 	$conexion = conectar();
 	if (isset($_POST['fecha'])) {
@@ -9,6 +10,9 @@
 		$sql = "select * from temperaturasdiarias";
 	}	
 	$resultado = mysqli_query($conexion, $sql);
+
+
+
 ?>
 <style>
 	table {
@@ -60,7 +64,6 @@ tbody td, thead th {
 			        	<td><?= $fecha; ?></td>
 			        </tr>
 					<?php
-					 
 					 endwhile;
 					 endif; 				
 					mysqli_close($conexion);			
@@ -82,7 +85,42 @@ tbody td, thead th {
 				<div class="w-100"></div>
 				<div class="col-md-12 border">
 					<h4>Datos de interes:</h4>
-					<div id="seccion"></div>
+					<?php
+						$dia = date("d");
+						$mes = date("m");
+						
+						$fecha_actual = date("d-m-Y");
+						$ayer = date("d",strtotime($fecha_actual."- 1 days")); 
+						$maniana = date("d",strtotime($fecha_actual."+ 1 days")); 
+
+						$conex = conectar();
+						$sqlPromedio = "select ROUND(AVG(Temperatura),2) AS tempProm, ROUND(AVG(humedad),2) AS humedadProm, ROUND(AVG(luz),2) AS luzProm from temperaturasdiarias WHERE MONTH(HoraRegistro) = $mes and DAY(HoraRegistro) = $dia ";
+						$resultadoPromedio = mysqli_query($conex, $sqlPromedio); 
+						$prom = mysqli_fetch_array($resultadoPromedio);
+						////////////////////////////////////////////////////////
+						$conexi = conectar();
+						$sqlPromedioAyer = "select ROUND(AVG(Temperatura),2) AS tempPromAyer from temperaturasdiarias WHERE MONTH(HoraRegistro) = $mes and DAY(HoraRegistro) = $ayer";
+						$resultadoPromedioAyer = mysqli_query($conexi, $sqlPromedioAyer); 
+						$promAyer = mysqli_fetch_array($resultadoPromedioAyer);
+						///////////////////////////////////////////////////////
+						//reduccion
+						$restaA = (-1 * intval($ayer)) + intval($dia);
+						$restaTemps = -1* $promAyer['tempPromAyer'] + $prom['tempProm'];
+						$a = $restaTemps / $restaA;
+						//fin reduccion
+						//sustitucion de a en ecuacion
+						$multiplicacion = $a * $dia;
+						$b = $prom['tempProm'] - $multiplicacion;
+						$prediccion = ($a * $maniana)+$b;
+						///////////////////////////////////////////////////////
+
+					?>
+					<div id="seccion" class="border">
+					 	<p>Promedio de temperatura de hoy :  <?= $prom['tempProm'];?>°C </p>
+					 	<p>Promedio de humedad de hoy :  <?= $prom['humedadProm'];?> </p>
+					 	<p>Promedio de luz de hoy :  <?= $prom['luzProm'];?> </p>
+					 	<p>Predicción para mañana: <?=$prediccion ?>°C</p>
+					</div>
 				</div>
 			</div>
 		</div>
